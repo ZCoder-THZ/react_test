a0e4d30465466f94a1c10a402d110dbe6c4b93ba
iamgroot404 <thanhtikezaw919@gmail.com>
2023-10-02 23:07:25 +0630

    feats:delete-task,ui

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,17 @@ function App() {
   let [todos, setTodos] = useState([
     {
       task: 'cutting grass',
-      time: '12:00am'
+      time: '12:00am',
+      id: 1
     },
     {
       task: 'mowing the lawn',
-      time: '1:00am'
+      time: '1:00am',
+      id: 2
     }, {
       task: 'go to fucking gym',
-      time: '2:00am'
+      time: '2:00am',
+      id: 3
     }
 
   ])
@@ -34,35 +37,61 @@ function App() {
   const handleTodo = () => {
 
 
-    setTodos([...todos, { task: text }]);
-    setText('');
+    // Remove white spaces using regex
 
 
+    // Count the length of the modified string
+    const textLength = text.replace(/\s+/g, "").length;
+
+    if (textLength) {
+      setTodos([...todos, { task: text, id: todos.length + 1 }]);
+      setText('');
+
+    } else {
+      alert('nope')
+    }
+
+
+
+  }
+  const removeList = (id) => {
+
+    const task = todos.filter(i => i.id !== id);
+    setTodos(task);
 
   }
 
   return (
     <>
-      <div className="container w-[400px] mx-auto bg-gray-400 p-4 rounded-md mt-40">
-        <h3 className='text-green-300 bg-black'>{text}</h3>
-        <input className=' b-3' type="text" value={text} onChange={() => {
-          setText(event.target.value)
-        }}
+      <div className="rounded-md mt-32 container mx-auto flex justify-center flex-col w-[130%]">
+        <div className="px-4 py-7 mx-auto border">
+          <h1 className="text-center mt-4 text-3xl ">To Do List</h1>
 
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              handleTodo()
+          <div className="flex align-center ">
+            <input className='bg-green-100 py-1' type="text" value={text} onChange={() => {
+              setText(event.target.value)
+            }}
+
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  handleTodo()
+                }
+              }}
+            />
+            <button className='py-1 px-3 text-white bg-green-400' onClick={handleTodo}>setTask</button>
+          </div>
+
+
+
+          <ul className='list-none mt-5  flex flex-col justify-content-between align-center'>
+            {
+              todos.map((todo) => <li key={todo.id} className='flex align-center justify-between my-2'>
+                <h3 className=''>{todo.task}</h3>
+                <button className='ml-5  block  bg-red-400 text-white py-1 px-3 rounded-md' onClick={() => removeList(todo.id)}>remove </button> </li>)
             }
-          }}
-        />
-        <button className='bg-blue-400 text-white px-2 rounded-md' onClick={handleTodo}>setTask</button>
+          </ul>
 
-
-        <ul>
-          {
-            todos.map((todo, i) => <li key={i}>{todo.task}</li>)
-          }
-        </ul>
+        </div>
 
       </div>
     </>
